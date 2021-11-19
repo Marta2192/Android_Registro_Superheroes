5bd93fb5892b727f6a33033cd21293a171bbecc8
MartaGago <martagago92@hotmail.com>
2021-11-19 13:52:56 +0100

    Diseño segunda pantalla - primeros pasos

---
+++ app/src/main/java/com/liceolapaz/dam/registrosuperhroes/EnDetalle.java
@@ -0,0 +1,14 @@
+package com.liceolapaz.dam.registrosuperhroes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EnDetalle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_en_detalle);
+    }
+}
