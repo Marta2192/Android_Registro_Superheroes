b0e02d29a2cac711599a545deb4db893ea773a0d
MartaGago <martagago92@hotmail.com>
2021-11-21 20:45:18 +0100

    Pasar info a traves de intents

--- app/src/main/java/com/liceolapaz/dam/registrosuperhroes/EnDetalle.java
+++ app/src/main/java/com/liceolapaz/dam/registrosuperhroes/EnDetalle.java
@@ -4,11 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.liceolapaz.dam.registrosuperhroes.databinding.ActivityEnDetalleBinding;
+import com.liceolapaz.dam.registrosuperhroes.databinding.ActivityMainBinding;
+
 public class EnDetalle extends AppCompatActivity {
+
+    public static final String SUPERHEROE_NOME = "superheroe_nome";
+    public static final String SUPERHEROE_EGO = "superheroe_ego";
+    public static final String SUPERHEROE_DESCRIPCION = "superheroe_descripcion";
+    public static final String SUPERHEROE_RATIO = "superheroe_ratio";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_en_detalle);
+        ActivityEnDetalleBinding binding = ActivityEnDetalleBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Bundle extras = getIntent().getExtras();
+
+        String nome = extras.getString(SUPERHEROE_NOME);
+        String alterEgo = extras.getString(SUPERHEROE_EGO);
+        String bio = extras.getString(SUPERHEROE_DESCRIPCION);
+        float rating = extras.getFloat(SUPERHEROE_RATIO);
+
+        binding.nameTxt.setText(nome);
+        binding.egoText.setText(alterEgo);
+        binding.bioText.setText(bio);
+        binding.ratingBar.setRating(rating);
+
     }
 }
--- app/src/main/java/com/liceolapaz/dam/registrosuperhroes/MainActivity.java
+++ app/src/main/java/com/liceolapaz/dam/registrosuperhroes/MainActivity.java
@@ -1,13 +1,53 @@
 package com.liceolapaz.dam.registrosuperhroes;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+import com.liceolapaz.dam.registrosuperhroes.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btGuardar.setOnClickListener(view -> {
+            String nome = binding.nome.getText().toString();
+            String alterEgo = binding.alterEgo.getText().toString();
+            String descripcion = binding.descripcion.getText().toString();
+            float ratio = binding.ratio.getRating();
+
+
+
+            openEnDetalle(nome, alterEgo, descripcion, ratio);
+        });
+
+
+
+        /*Button btguardar = findViewById(R.id.bt_guardar);
+        EditText nome = findViewById(R.id.nome);
+        EditText alterEgo = findViewById(R.id.alterEgo);
+        EditText descripcion = findViewById(R.id.descripcion);
+        RatingBar ratio = findViewById(R.id.ratio);
+*/
+
+
+    }
+
+    private void openEnDetalle(String nome, String alterEgo, String descripcion, float ratio){
+        Intent intent = new Intent(this, EnDetalle.class);
+        intent.putExtra(EnDetalle.SUPERHEROE_NOME, nome);
+        intent.putExtra(EnDetalle.SUPERHEROE_EGO, alterEgo);
+        intent.putExtra(EnDetalle.SUPERHEROE_DESCRIPCION, descripcion);
+        intent.putExtra(EnDetalle.SUPERHEROE_RATIO, ratio);
+        startActivity(intent);
     }
 }
